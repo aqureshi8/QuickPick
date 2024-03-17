ecc66744d281d929f4814beca472aead73aaf78a
Ahsan <ahsan.qureshi8@gmail.com>
2024-03-16 21:48:43 -0400

    Initial Commit

---
+++ QuickPick/QuickPickApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickPickApp.swift
+//  QuickPick
+//
+//  Created by Ahsan Qureshi on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct QuickPickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
